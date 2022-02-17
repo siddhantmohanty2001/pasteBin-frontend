4645d34ba2faca06b6f0352af539c17880ab885c
Siddhant Mohanty <siddhantmohanty2001@gmail.com>
2022-02-18 01:56:34 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import TextBox from "./components/TextBox";
 import Navbar from "./components/Navbar"
+// import {HashRouter as Router,Route,Switch,Routes} from "react-router-dom" 
 import {BrowserRouter as Router,Route,Switch,Routes} from "react-router-dom" 
 import Home from "./components/Home"
 import Display from "./components/Display"
@@ -12,7 +13,7 @@ function App() {
       <Navbar/>
       <Router>
         <Routes>
-    <Route path="/" element={<Home/>}></Route>
+    <Route path="/" exact element={<Home/>}></Route>
     <Route path="/new" element={<TextBox/>}></Route>
     <Route path="/display" element={<Display/>}></Route>
     <Route path="/edit" element={<Edit/>}></Route>
